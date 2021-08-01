aa57edb1eea8259738c8a7954b84f482fca6b2b2
Trà Hữu Minh <trahuuminh@gmail.com>
2021-08-01 17:14:22 +0700

    setup base entity

---
+++ src/main/java/cybersoft/java12/gira/common/entity/BaseEntity.java
@@ -0,0 +1,24 @@
+package cybersoft.java12.gira.common.entity;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+import javax.persistence.Version;
+
+@MappedSuperclass
+public class BaseEntity {
+	@Id
+	@GeneratedValue
+	@Column(updatable = false)
+	protected Long id;
+	
+	@Version
+	protected int version;
+	protected String createdBy;
+	protected LocalDateTime createdAt;
+	protected String updateBy;
+	protected LocalDateTime updatedAt;
+}
