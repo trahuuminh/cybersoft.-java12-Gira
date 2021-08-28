68247ee4eaf662ef2e0b4cc9d0eb17ea8a3a515e
Trà Hữu Minh <trahuuminh@gmail.com>
2021-08-28 17:23:48 +0700

    complete authentication

--- src/main/java/cybersoft/java12/gira/security/config/SecurityConfig.java
+++ src/main/java/cybersoft/java12/gira/security/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package cybersoft.java12.gira.security.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -11,15 +13,21 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import cybersoft.java12.gira.security.jwt.JwtAuthorizationFilter;
+
+
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+	@Autowired
+	@Qualifier("userDetailsServiceImpl")
 	private UserDetailsService userDetailsService;
 	
-	public SecurityConfig(UserDetailsService service) {
-		userDetailsService=service;
-	}
+	@Autowired
+	private JwtAuthorizationFilter jwtAuthorizationFilter;
 	
 	@Bean
 	public PasswordEncoder getPasswordEncoder() {
@@ -27,29 +35,35 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	}
 	
 	@Bean
-	public AuthenticationManager authenticationManagerBean() throws Exception{
+	@Override
+	public AuthenticationManager authenticationManagerBean() throws Exception {
 		return super.authenticationManagerBean();
 	}
 
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.userDetailsService(userDetailsService).passwordEncoder(getPasswordEncoder());
+		auth.userDetailsService(userDetailsService)
+			.passwordEncoder(getPasswordEncoder());
 	}
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		//cấu hình CORS
+		// cấu hình CORS
 		http.cors();
 		
-		//cấu hình session
+		// cấu hình session
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 		
-		//disable csrf
+		// thêm filter để validate jwt token
+		http.addFilterBefore(jwtAuthorizationFilter, UsernamePasswordAuthenticationFilter.class);
+		
+		// disable csrf
 		http.csrf().disable();
 		
-		//cấu hình xác thực cho các API
-		http.antMatcher("/api/**").authorizeRequests().antMatchers("/api/user").permitAll().anyRequest().authenticated();
-		
+		// cấu hình xác thực cho các api
+		http.antMatcher("/api/**").authorizeRequests()
+			.antMatchers("/api/auth/login").permitAll()
+			.anyRequest().authenticated();
 		
 	}
 	
